b9fe8f1d9d15a63d4c71bc886edcdfc911156fc1
kmilaz14 <kamilazhwa@gmail.com>
2024-10-10 11:11:16 +0800

    tugas 4 percobaan 1

--- SiakadFor13.java
+++ SiakadFor13.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 
-public class SiakadFor13 { //maaf bu vivi saya lupa absen saya berubah
+public class SiakadFor13 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double nilai, tertinggi=0, terendah=100;
+        int lulus = 0, tdkLulus = 0;
 
         for (int i=1; i<=10; i++){
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -11,12 +12,19 @@ public class SiakadFor13 { //maaf bu vivi saya lupa absen saya berubah
             if (nilai > tertinggi) {
                 tertinggi = nilai;
             }
+            
             if (nilai < terendah){
                 terendah = nilai;
-            } 
+            } if (nilai >= 60){
+                lulus ++;
+            } else {
+                tdkLulus ++;
+            }
         }
 
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("Jumlah mahasiswa lulus: " + lulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " + tdkLulus);
     }
 }
